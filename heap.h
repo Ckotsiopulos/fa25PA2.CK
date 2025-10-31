0b67ddbc9f1c8070bd9fc6041f6a52a591b788ea
Christos Kotsiopulos <ckotsiopulos5746@sdsu.edu>
2025-10-30 23:32:40 -0700

    Finished the Push and Pop functions for the MinHeap structure in heap.h.

--- heap.h
+++ heap.h
@@ -16,12 +16,23 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+         if (size >= 64) return; //If the heap is full, do nothing.
+        data[size] = idx; //insert data at end
+        upheap(size, weightArr); //Upheap it to restore the order of the heap.
+        size++; //Increase heap size/increment.
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        if (size == 0) return -1;
+        int root = data[0];//This is the root value.
+        size--;//Decrement size.
+        if (size > 0) { //Move the last element to the root and now downheap.
+            data[0] = data[size];
+            downheap(0, weightArr);
+        }
+        return root; //return the value of root.
     }
 
     void upheap(int pos, int weightArr[]) {
